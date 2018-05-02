8ad4f7564840306fa230573f695f8ac4fb14ad68
alexs222 <aleks.nikulin@gmail.com>
2018-05-02 22:51:52 +0200

    starting modification project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  #app
+  
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    main: './src/main.js',    
+    styles: './src/styles/index.js'
+  },
+  
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
